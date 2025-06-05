b25ede7128e083c170bf520a03a72998296805e9
prem0331 <premkumarreddy086@gmail.com>
2025-06-05 11:03:15 +0530

    Delete URL Shortener/routes/user.js

--- URL Shortener/routes/user.js
+++
@@ -1,6 +0,0 @@
-const express=require("express");
-const {handleusersignup, handleuserlogin}=require("../controllers/user");
-const router=express.Router();
-router.post("/",handleusersignup);
-router.post("/login",handleuserlogin );
-module.exports=router;
